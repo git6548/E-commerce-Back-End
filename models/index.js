c4ba14d4d73bc7f3151ff7ddb9784bf35c57d65c
Stephanie <stephanieanderson5133@gmail.com>
2022-03-19 14:40:31 -0500

    models and index complete

--- models/index.js
+++ models/index.js
@@ -6,19 +6,17 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  //not totally sure if i use id or something else
-  foreginKey: 'id',
+  foreginKey: 'category_id',
   OnDelete: 'CASCADE'
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  //not totally sure if i use id or something else
-  foreginKey: 'id',
+  foreginKey: 'category_id',
   onDelete: 'CASCADE'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-through: 'ProductTag'
+  through: 'ProductTag'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(ProductTag, {
