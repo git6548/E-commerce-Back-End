1ea6bc38f507eab1708c80e4ee8937f8af38a4ca
Stephanie <stephanieanderson5133@gmail.com>
2022-03-23 12:25:45 -0500

    working on routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,11 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+  });
 });
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
